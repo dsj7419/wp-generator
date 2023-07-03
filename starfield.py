b8f9fb6113e98e49f50fc6a02884945c871d2bdb
Dan Johnson <dsj7419@users.noreply.github.com>
2023-07-03 08:35:29 -0700

    Update starfield.py
    randomize size a bit, add color

--- starfield.py
+++ starfield.py
@@ -11,8 +11,23 @@ draw = ImageDraw.Draw(image)
 
 # Define parameters for the stars
 min_star_diameter = 1
-max_star_diameter = 3
-num_stars = 5000
+max_star_diameter = 5
+num_stars = 3000
+
+# Define colors for the stars (these are RGB tuples)
+colors = [(255, 255, 255), (255, 240, 220), (255, 220, 180), (180, 220, 255)]
+
+# Function to draw stars with a twinkle
+def draw_star(x, y, diameter, color, draw):
+    # Draw the star
+    draw.ellipse([(x, y), (x + diameter, y + diameter)], fill=color)
+
+    # Draw the twinkling effect
+    if diameter > 2:
+        for i in range(4):
+            dx = np.random.normal(scale=diameter)
+            dy = np.random.normal(scale=diameter)
+            draw.line([(x + dx, y + dy), (x + dx + diameter/2, y + dy + diameter/2)], fill=color)
 
 # Generate the stars
 for _ in range(num_stars):
@@ -20,9 +35,10 @@ for _ in range(num_stars):
     x_pos = np.random.randint(0, width)
     y_pos = np.random.randint(0, height)
     star_diameter = np.random.randint(min_star_diameter, max_star_diameter)
+    star_color = colors[star_diameter - min_star_diameter] # assign color based on size
 
     # Draw the star
-    draw.ellipse([(x_pos, y_pos), (x_pos + star_diameter, y_pos + star_diameter)], fill='white')
+    draw_star(x_pos, y_pos, star_diameter, star_color, draw)
 
 # Save the image
 image.save("background.jpeg")
